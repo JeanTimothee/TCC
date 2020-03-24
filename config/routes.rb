78810bd4f00190b5ce179832082ea4b9769c8d1c
Timothee Regis <registimothee@gmail.com>
2020-03-24 17:06:58 +0100

    remove teacher#show route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :teachers
+  resources :teachers, only: [:index, :new, :create, :update, :edit, :destroy]
   resources :lessons do
     resources :students, only: [:new, :create]
     resources :student_lessons
