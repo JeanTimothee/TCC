8d4981dc39e044ccb84779e6903bdb8e7a396ee0
Timothee Regis <registimothee@gmail.com>
2020-03-25 12:25:45 +0100

    added ttc.xml file

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,6 @@
 class Student < ApplicationRecord
-  LEVELS = (1..24).to_a
-
+  LEVELS = (1..26).to_a
+  # LEVELS = ["C-", "C", "CC+", "C+", "C+b-", "C+B-", "B-", "B-B", "B", "BB+", "B+", "A-", "A", "A 30/5", "A 30/4", "A 30/3", "A 30/2", "A 30/1", "A 30", "A 15/5", "A 15/4", "A 15/4", "A 15/3", "A 15/2", "A 15/1", "A 15"]
   belongs_to :referent
   has_many :availabilities
   has_many :preferences
