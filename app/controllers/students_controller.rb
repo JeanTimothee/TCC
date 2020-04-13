9e3a0634d5113be298ba7b1ed20af1e7933cf806
Timothee Regis <registimothee@gmail.com>
2020-04-14 00:00:51 +0200

    added pg search and conditions

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,33 +3,35 @@ class StudentsController < ApplicationController
   def index
     @lessons = Lesson.all
     @students = Student.all
-    @students_level_filter = []
-    @students_age_filter = []
 
     # search params
+    @students = Student.search_by_first_last_name(params[:query]) if params[:query].present?
     if params[:level].present?
       level_query = params[:level].map do |level|
         LEVEL_NUMBER[level]
       end
       @students = @students.where(level: level_query)
     end
-
-    if params[:age].present?
-        @students = @students.where(age: params[:age])
-    end
+    @students = @students.where(age: params[:age]) if params[:age].present?
+    @students = Student.all if @students.count == 0
 
     # filters collections
-    @ages = @students.map do |student|
-     student.age
-    end
-    @ages.uniq!.sort!
-    nb_levels = @students.map do |student|
-      student.level
-    end
-    nb_levels.uniq!.delete(nil)
-    @levels = nb_levels.sort!.map do |level|
-      NUMBER_LEVEL[level]
+    if @students.count > 1
+      @ages = @students.map do |student|
+       student.age
+      end
+      @ages.uniq!.sort!
+      nb_levels = @students.map do |student|
+        student.level
+      end
+      nb_levels.uniq!.delete(nil)
+      @levels = nb_levels.sort!.map do |level|
+        NUMBER_LEVEL[level]
+      end
+      @students = @students.order(:last_name)
+    else
+      @ages = @students[0].age
+      @levels = NUMBER_LEVEL[@students[0].level]
     end
   end
-
 end
